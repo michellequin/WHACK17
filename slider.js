e7765b99b441100240bad5d754d8231a7bb02756
ray10102 <huang.ra@husky.neu.edu>
2017-02-12 10:08:41 -0500

    slider data persistence

--- slider.js
+++ slider.js
@@ -1,8 +1,33 @@
-var isLearn = true;
-var isDebate = true;
-var topic;
+sessionStorage["isLearn"] = false;
 
-function othername() {
-    var input = document.getElementById("userInput").value;
-    alert(input);
+function saveTopic() {
+	if(document.getElementbyID("fem").value === true) {
+		sessionStorage["topic"] = "fem";
+	} else if(document.getElementbyID("blm").value === true) {
+		sessionStorage["topic"] = "blm";
+	} else if(document.getElementbyID("imm").value === true) {
+		sessionStorage["topic"] = "imm";
+	} else if(document.getElementbyID("wom").value === true) {
+		sessionStorage["topic"] = "wom";
+	} else {
+		sessionStorage["topic"] = "lgbt";
+	}
+}
+
+function setisLeftTrue() {
+	sessionStorage["isLeft"] = true;
+}
+
+function setisLeftfalse() {
+	sessionStorage["isLeft"] = false;]
+}
+
+function saveandTrue() {
+	saveTopic();
+	setisLeftTrue();
+}
+
+function saveandFalse() {
+	saveTopic();
+	setisLeftFalse();
 }
